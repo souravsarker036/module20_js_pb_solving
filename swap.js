235d8211b00639533c86f4f4b8c8744fe7fec6db
Sourav Sarker <souravsarker036@gmail.com>
2025-08-25 11:41:33 +0600

    swaping number

---
+++ swap.js
@@ -0,0 +1,19 @@
+let a = 5;
+let b = 7;
+
+console.log(a,b)
+// a= b;
+// b=a;
+
+const temp = a;
+a=b;
+b= temp;
+console.log(a,b)
+
+//---------------
+let x = 5;
+let y = 8;
+[x,y]=[y,x]
+console.log(x,y)
+
+//----
