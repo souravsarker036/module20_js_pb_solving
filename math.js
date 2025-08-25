9de2f7845541ff81a443263b240e6c6cc324ceee
Sourav Sarker <souravsarker036@gmail.com>
2025-08-25 11:05:16 +0600

    math shortcut

---
+++ math.js
@@ -0,0 +1,9 @@
+const min = Math.min(45,21,32,99,34)
+console.log(min)
+const max = Math.max(45,21,32,99,34)
+console.log(max)
+console.log(Math.abs())
+
+console.log(Math.round(5.22))
+console.log(Math.floor(5.92))
+console.log(Math.ceil(5.0002))
